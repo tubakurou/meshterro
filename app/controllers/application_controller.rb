6254521fdb7a47d8f72fa5c70db6043dc8008939
EC2 Default User <ec2-user@ip-172-31-58-182.ec2.internal>
2021-12-06 01:46:24 +0000

    ７章

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
-before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!,except: [:top]
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-def after_sign_in_path_for(resource)
+ def after_sign_in_path_for(resource)
     post_images_path
   end
   protected
@@ -9,4 +10,4 @@ def after_sign_in_path_for(resource)
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
- end
+  end
